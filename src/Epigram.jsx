b3309f7dac64b6de6f550283e54b474e84d31f3f
zfletch <zfletch2@gmail.com>
2024-11-02 14:27:01 -0700

    get horizontal highlighting to work

--- src/Epigram.jsx
+++ src/Epigram.jsx
@@ -1,8 +1,43 @@
 import Accordion from 'react-bootstrap/Accordion';
 import Container from 'react-bootstrap/Container';
 
+function horizontalDisplay(lines, hlStartIndex, size) {
+  let count = 0;
+
+  return lines.map((line) => {
+    const words = line.split(' ');
+
+    return (
+      <p>
+        {words.map((word, index) => {
+          let text;
+          if (count >= hlStartIndex && count < hlStartIndex + size) {
+            text = (
+              <span>
+                <strong>{word[0]}</strong>{word.slice(1)}
+                {(index < words.length - 1) && ' '}
+              </span>
+            );
+          } else {
+            text = (
+              <span>
+                {word}
+                {(index < words.length - 1) && ' '}
+              </span>
+            );
+          }
+
+          count++;
+
+          return text;
+        })}
+      </p>
+    );
+  });
+}
+
 function Epigram({ eventKey, word, vertical, epigram: { name, url, lines, line_letters, word_letters } }) {
-  const hl_start_index = vertical ? line_letters.join('').indexOf(word) : word_letters.join('').indexOf(word);
+  const hlStartIndex = vertical ? line_letters.join('').indexOf(word) : word_letters.join('').indexOf(word);
 
   return (
     <Accordion.Item eventKey={eventKey}>
@@ -12,13 +47,11 @@ function Epigram({ eventKey, word, vertical, epigram: { name, url, lines, line_l
       <Accordion.Body>
         <div>
           {vertical && lines.map((line, index) => (
-            (index >= hl_start_index && index < hl_start_index + word.length)
+            (index >= hlStartIndex && index < hlStartIndex + word.length)
               ? <p key={index}><strong>{line[0]}</strong>{line.slice(1)}</p>
               : <p key={index}>{line}</p>
           ))}
-          {!vertical && lines.map((line, index) => (
-            <p key={index}>{line}</p>
-          ))}
+          {!vertical && horizontalDisplay(lines, hlStartIndex, word.length)}
         </div>
       </Accordion.Body>
     </Accordion.Item>
